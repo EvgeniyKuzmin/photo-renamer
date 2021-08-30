f11ccf1deab882b04ca89820455fea953c255b0f
EvgeniyKuzmin <evgeniy.a.kuzmin@gmail.com>
2021-08-30 23:27:28 +0300

    Upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='photo_renamer',
-    version='0.1.0',
+    version='0.1.1',
     description=(
         'Enables you making collections of unique JPEG-photos '
         'with names are formatted by a specific template'
